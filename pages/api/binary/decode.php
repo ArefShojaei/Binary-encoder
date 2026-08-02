7662a33fe51476b1b03f0240c6b85a2b0c3288fc
ArefShojaei <arefshojaei82@gmail.com>
2026-08-02 16:11:11 +0330

    feat: add Binary api end-points

---
+++ pages/api/binary/decode.php
@@ -0,0 +1,16 @@
+<?php
+
+use Routex\Http\{Request, Response};
+
+use App\Controllers\BinaryController;
+use App\Middlewares\OnlyPostMiddleware;
+
+$request = Request::capture();
+$response = new Response();
+
+$middleware = new OnlyPostMiddleware();
+
+$middleware->handle($request, $response, function ($request, $response) {
+    $controller = new BinaryController();
+    $controller->decode($request, $response);
+});
